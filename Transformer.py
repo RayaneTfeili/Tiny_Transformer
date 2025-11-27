dcad185716b9203a02214fccc34dff3e7c2797f4
Rayane <rayantfeili@gmail.com>
2025-11-27 16:43:56 +0100

    Encoder & part of decoder layer

--- Transformer.py
+++ Transformer.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import math 
+import torch.functional as F 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class Embedding(nn.Module):
@@ -54,42 +55,38 @@ class FFNN(nn.Module):
         return self.drop(self.fc2(self.drop(self.act(self.fc1(input))))) 
     
 class Attention(nn.Module):
-    def __init__(self, d_model: int, d_head: int, dropout: float):
+    def __init__(self, d_model: int, d_head: int, dropout: float, masked : bool):
         super().__init__()
         self.d_model = d_model
         self.d_head = d_head
-
+        self.masked_default = masked 
         self.query_layer = nn.Linear(d_model, d_head, bias=True)
         self.key_layer   = nn.Linear(d_model, d_head, bias=True)
         self.value_layer = nn.Linear(d_model, d_head, bias=True)
 
         self.dropout_layer = nn.Dropout(dropout)
 
-    def forward(self, x : torch.Tensor, masked = False):
-        """
-        x: [batch_size, seq_len, d_model]
-        return: [batch_size, seq_len, d_head]
-        """
+    def forward(self, x : torch.Tensor, masked :bool = None):
+       
         batch_size, seq_len, model_dim  = x.size()
         assert model_dim == self.d_model , f"Input dimension {model_dim} doesn't match the model dimension {self.d_model}"
 
-        query = self.query_layer(x)   # [B, S, d_head]
-        key   = self.key_layer(x)     # [B, S, d_head]
-        value = self.value_layer(x)   # [B, S, d_head]
+        query = self.query_layer(x)  
+        key   = self.key_layer(x)     
+        value = self.value_layer(x) 
 
-        # [B, S, d_head] @ [B, d_head, S] -> [B, S, S]
         att_scores = (query @ key.transpose(-2, -1)) * (1.0 / math.sqrt(self.d_head))
 
        
-
+        if masked is None:
+            masked = self.masked_default
         if masked:
-            # matrice [S, S] avec 1 au-dessus de la diagonale
-            # True la ou on DOIT MASQUER (futur)
+            
             causal_mask = torch.triu(
                 torch.ones(seq_len, seq_len, device=x.device, dtype=torch.bool),
                 diagonal=1
             )
-            # on remplace ces positions par -inf avant softmax
+           
             att_scores = att_scores.masked_fill(causal_mask, float("-inf"))
 
 
@@ -100,7 +97,7 @@ class Attention(nn.Module):
 
 
 class MHA(nn.Module):
-    def __init__(self, d_model: int, num_heads: int, dropout: float):
+    def __init__(self, d_model: int, num_heads: int, dropout: float,masked: bool = False):
         super().__init__()
         assert d_model % num_heads == 0, f"d_model {d_model} must be divisible by num_heads {num_heads}"
         self.d_model = d_model
@@ -108,22 +105,65 @@ class MHA(nn.Module):
         self.d_head = d_model // num_heads
 
         self.heads = nn.ModuleList([
-            Attention(d_model=self.d_model, d_head=self.d_head, dropout=dropout)
+            Attention(d_model=self.d_model, d_head=self.d_head, dropout=dropout, masked = masked)
             for _ in range(self.num_heads)
         ])
 
         self.output_projection = nn.Linear(self.num_heads * self.d_head, self.d_model)
-        self.dropout_layer = nn.Dropout(dropout)
+        self.drop = nn.Dropout(dropout)
 
-    def forward(self, x: torch.Tensor,masked = False) -> torch.Tensor:
-        """
-        x: [B, S, d_model]
-        return: [B, S, d_model]
-        """
-        # liste de [B, S, d_head] -> concat [B, S, num_heads*d_head]
-        head_outputs = [h(x,masked) for h in self.heads]
+    def forward(self, x: torch.Tensor,masked : bool = None ):
+        
+        head_outputs = [h(x,masked = masked ) for h in self.heads]
         output = torch.cat(head_outputs, dim=-1)
-        output = self.output_projection(output)   # [B, S, d_model]
+        output = self.output_projection(output)  
         output = self.dropout_layer(output)
         return output
     
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model : int, num_heads : int ,d_hidden : int, dropout : float, masked : bool = False):
+        super().__init__()
+        
+        self.att_layer = MHA(d_model = d_model, num_heads = num_heads, dropout = dropout, masked = masked)
+        self.feed_forward_layer = FFNN(d_model = d_model, d_hidden=  d_hidden,dropout = dropout)
+        self.drop = nn.Dropout(dropout)
+        self.LayerNorm_att = nn.LayerNorm(d_model)
+        self.LayerNorm_ffnn = nn.LayerNorm(d_model)
+
+    def forward(self,embed_input : torch.Tensor ,masked : bool  = None):
+        att_sublayer = self.att_layer(self.LayerNorm_att(embed_input), masked = masked  )
+        x = embed_input + self.dropout(att_sublayer)
+
+
+        ffnn_sublayer = self.feed_forward_layer(self.LayerNorm_ffnn(x)) 
+        x = x +  self.dropout(ffnn_sublayer)
+        
+
+        return x
+
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model : int, num_heads : int ,d_hidden : int, dropout : float, masked : bool = True):
+        super().__init__()
+        
+        self.att_layer = MHA(d_model = d_model, num_heads = num_heads, dropout = dropout, masked = masked)
+        self.feed_forward_layer = FFNN(d_model = d_model, d_hidden=  d_hidden,dropout = dropout)
+        self.drop = nn.Dropout(dropout)
+        self.LayerNorm_att = nn.LayerNorm(d_model)
+        self.LayerNorm_att2 = nn.LayerNorm(d_model)
+        self.LayerNorm_ffnn = nn.LayerNorm(d_model)
+
+    def forward(self,embed_input : torch.Tensor ,masked : bool  = True):
+
+        att_sublayer = self.att_layer(self.LayerNorm_att(embed_input), masked = masked  )
+        x = embed_input + self.dropout(att_sublayer)
+
+        att_sublayer2 = self.att_layer(self.LayerNorm_att2(embed_input), masked = False  )
+        x = x + self.dropout(att_sublayer2)
+
+        ffnn_sublayer = self.feed_forward_layer(self.LayerNorm_ffnn(x)) 
+        x = x +  self.dropout(ffnn_sublayer)
+        
+        return x 
+
+
